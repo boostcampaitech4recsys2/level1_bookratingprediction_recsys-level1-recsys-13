9c5e889826a95b303eaf3184fd7e275eaf347e18
7dudtj <7dudtj@naver.com>
2022-11-10 09:02:10 +0000

    Fix: Delete unused codes for escaping collisions

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import pandas as pd
 
 from src import seed_everything
 
-from src.data import context_data_load, context_data_split, context_data_loader
 from src.data import fm_data_load, fm_data_split, fm_data_loader
 from src.data import ffm_data_load, ffm_data_split, ffm_data_loader
 from src.data import dl_data_load, dl_data_split, dl_data_loader
 from src.data import ncf_data_load, ncf_data_split, ncf_data_loader
-from src.data import dcn_data_load, dcn_data_split, dcn_data_loader
 from src.data import wdn_data_load, wdn_data_split, wdn_data_loader
 
 from src.data import image_data_load, image_data_split, image_data_loader
